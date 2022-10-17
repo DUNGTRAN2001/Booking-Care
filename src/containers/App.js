affe972638a72537e5af5998efd7dc8b432285f2
= <=>
2022-10-17 23:40:13 +0700

    design header homepage

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,8 +19,7 @@ import Header from "./Header/Header";
 import System from "../routes/System";
 
 import { CustomToastCloseButton } from "../components/CustomToast";
-import ConfirmModal from "../components/ConfirmModal";
-
+import HomePage from "./HomePage/HomePage.js";
 class App extends Component {
   handlePersistorState = () => {
     const { persistor } = this.props;
@@ -45,7 +44,6 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            <ConfirmModal />
             {this.props.isLoggedIn && <Header />}
 
             <span className="content-container">
@@ -59,6 +57,7 @@ class App extends Component {
                   path={path.SYSTEM}
                   component={userIsAuthenticated(System)}
                 />
+                <Route path={path.HOMEPAGE} component={HomePage} />
               </Switch>
             </span>
 
